78aabe43dff5ac2c8dd1953c3c6f00b9ee907014
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-01 15:01:54 +0300

    Выполнил усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,12 +1,24 @@
-let num = 266219;
-let str = String(num);
-let sum = 0;
-for (let i = 0; i < str.length; i++) {
-  sum += +str[i];
+let lang = null;
+if (lang === 'ru') {
+  console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+} else if (lang === 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 }
-sum **= 3;
-let strSum =String(sum);
-let firstNumber = strSum[0];
-let secondNumber = strSum[1];
-console.log(firstNumber);
-console.log(secondNumber);
+
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+    break;
+  case 'en':
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+
+let arr = ['en', 'ru'];
+let firstElement = arr[0];
+let result = (firstElement === 'en') ? console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday') :
+console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+
+let namePerson = null;
+  let answer = (namePerson === 'Артем') ? console.log('директор'):
+  (namePerson === 'Максим') ? console.log('преподаватель'):
+  console.log('студент');
