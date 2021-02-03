55a91874ed8b5a424942eaf7ab04ed0604a3cf27
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-03 15:37:23 +0300

    Выполнил усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,27 +1,11 @@
-let lang = null;
-if (lang === 'ru') {
-  console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-} else if (lang === 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}
-
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-    break;
-  case 'en':
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}
-
-let arr = [];
-arr.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-arr.ru = ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-let question = prompt('Введите язык недели, "en", "ru"');
-let result = (question === 'en') ? console.log(arr.en) :
-             (question === 'ru') ?  console.log(arr.ru):
-             console.log('Неизвестный язык');
-             
-let namePerson = null;
-  let answer = (namePerson === 'Артем') ? console.log('директор'):
-  (namePerson === 'Максим') ? console.log('преподаватель'):
-  console.log('студент');
+'use strict';
+let getString = function(element) {
+  if(typeof(element) !== 'string') {
+    return 'Введите строку';
+  } 
+  element = element.trim();
+  if(element.length > 30) {
+    return element.substring(0, 30) + ' ...';
+  }
+};
+getString();
