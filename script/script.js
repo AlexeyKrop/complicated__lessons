38818cea07afb9f2b18bad0254ceab8b24e8c78f
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-05 15:26:16 +0300

    Выполнил усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,11 +1,24 @@
 'use strict';
-let getString = function(element) {
-  if(typeof(element) !== 'string') {
-    return 'Введите строку';
-  } 
-  element = element.trim();
-  if(element.length > 30) {
-    return element.substring(0, 30) + ' ...';
+let arr = ['32','22','12','2','4','5','64'];
+for(let i = 0; i < arr.length; i++){
+if(arr[i].startsWith('2') || arr[i].startsWith('4')) {
+    console.log(arr[i]);
   }
-};
-getString();
+}
+
+
+let n = 100;
+let count = 0;
+
+for(let i = 2; i <= n; i++){
+  for(let j = 1; j <= i; j++){
+    if(i % j === 0){
+      count++;
+    }
+    }
+    if(count === 2){
+      console.log('Делители числа ' + i + ': 1' + ' и ' + i);
+    }
+    count = 0;
+  }
+
