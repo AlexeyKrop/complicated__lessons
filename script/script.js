caea351c7c56616bb55b9a73f4e083375d9673c6
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-11 15:57:11 +0300

    Исправил

--- script/script.js
+++ script/script.js
@@ -1,15 +1,17 @@
 'use strict';
-let week = ["Понедельник\n", `Вторник\n`, "Среда\n", "Четверг\n", "Пятница\n", "Суббота\n", "Воскресенье\n"];
+let week = ['Понедельник \n', `Вторник \n`, "Среда \n", "Четверг\n", "Пятница\n", "Суббота\n", "Воскресенье\n"];
 let now = new Date();
 let today = (now.getDay() - 1);
+
 for (let i = 0; i < week.length; i++){
   if (i === 5 || i === 6) {
-    console.log(week[i].italics());
+    document.write(week[i].italics());
   } else if (i === today) {
-    console.log(week[i].bold());
+    document.write(week[i].bold());
+  } else {
+    document.write(week[i]);
   }
 }
-console.log(week.join(''));
 
 
 
