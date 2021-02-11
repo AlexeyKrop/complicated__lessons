e14cc1c185bed802aa2e92312c13c9f641432b91
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-11 20:04:06 +0300

    Исправил ошибки

--- script/script.js
+++ script/script.js
@@ -2,15 +2,15 @@
 let week = ['Понедельник \n', `Вторник \n`, "Среда \n", "Четверг\n", "Пятница\n", "Суббота\n", "Воскресенье\n"];
 let now = new Date();
 let today = (now.getDay() - 1);
-
 for (let i = 0; i < week.length; i++){
   if (i === 5 || i === 6) {
-    document.write(week[i].italics());
+    document.write(`<p><i>${week[i]}</i></p>`);
   } else if (i === today) {
-    document.write(week[i].bold());
+    document.write(`<p><b>${week[i]}</b></p>`);
   } else {
-    document.write(week[i]);
+    document.write(`<p>${week[i]}</p>`);
   }
+  
 }
 
 
