ac6cb499d35e0393031d8e56cc21d049a20e44fc
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-11 10:16:43 +0300

    Усложненное задание

--- script/script.js
+++ script/script.js
@@ -1,24 +1,17 @@
 'use strict';
-let arr = ['32','22','12','2','4','5','64'];
-for(let i = 0; i < arr.length; i++){
-if(arr[i].startsWith('2') || arr[i].startsWith('4')) {
-    console.log(arr[i]);
+let week = ["Понедельник\n", `Вторник\n`, "Среда\n", "Четверг\n", "Пятница\n", "Суббота\n", "Воскресенье\n"];
+let now = new Date();
+let today = (now.getDay() - 1);
+for (let i = 0; i < week.length; i++){
+  if (i === 5 || i === 6) {
+    console.log(week[i].italics());
+  } else if (i === today) {
+    console.log(week[i].bold());
   }
 }
+console.log(week.join(''));
 
 
-let n = 110;
-let count = 0;
 
-for(let i = 2; i <= n; i++){
-  for(let j = 1; j <= i; j++){
-    if(i % j === 0){
-      count++;
-    }
-    }
-    if(count === 2){
-      console.log('Делители числа ' + i + ': 1' + ' и ' + i);
-    }
-    count = 0;
-  }
 
+ 
